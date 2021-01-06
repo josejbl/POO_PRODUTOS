8eb2c18ad1019e5e3f63139df24a76c50ee3089a
FIC <FIC@info.local>
2021-01-06 10:57:33 -0300

    TrabalhoAT

---
+++ Classes.cs/Marca.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace POO_PRODUTOS.Classes.cs
+{
+    public class Marca
+    {
+        int Codigo { get; set; }
+        string NomeMarca { get; set; }
+        DateTime DataCadrasto { get; set; }
+
+        List<Marca> Marcas { get; set; }
+        public string CadastrarMarca(Marca marca){
+
+            return "";
+            
+        }
+        public void lstar(){
+
+        }
+        public void Deletar(){
+
+        }
+        
+        
+        
+        
+    }
+}
